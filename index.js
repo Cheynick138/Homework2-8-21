7b500e501b6149b829a215a5660d51e21fe51764
Cheyenne Nickelberry <30728389+Cheynick138@users.noreply.github.com>
2021-02-08 18:44:17 -0500

    Update index.js

--- index.js
+++ index.js
@@ -4,3 +4,17 @@ const util = require("util");
 const inquirer = require("inquirer");
 const generateReadme = require("./utils/generateReadme")
 const writeFileAsync = util.promisify(fs.writeFile);
+
+//Prompt the user questions to populate the README.md
+function promptUser(){
+    return inquirer.prompt([
+        {
+            type: "input",
+            name: "projectTitle",
+            message: "What is the project title?",
+        },
+        {
+            type: "input",
+            name: "description",
+            message: "Write a brief description of your project: "
+        },
