d48c00bc1d5304995349b10c456bddc132b9c165
Cheyenne Nickelberry <30728389+Cheynick138@users.noreply.github.com>
2021-02-08 18:55:48 -0500

    Update index.js

--- index.js
+++ index.js
@@ -18,3 +18,32 @@ function promptUser(){
             name: "description",
             message: "Write a brief description of your project: "
         },
+ {
+            type: "input",
+            name: "installation",
+            message: "Describe the installation process if any: ",
+        },
+        {
+            type: "input",
+            name: "usage",
+            message: "What is this project usage for?"
+        },
+        {
+            type: "list",
+            name: "license",
+            message: "Chose the appropriate license for this project: ",
+            choices: [
+                "Apache",
+                "Academic",
+                "GNU",
+                "ISC",
+                "MIT",
+                "Mozilla",
+                "Open"
+            ]
+        },
+        {
+            type: "input",
+            name: "contributing",
+            message: "Who are the contributors of this projects?"
+        },
